59d5af417a2ef51ebf75947ba056e46c528a7484
Libra420T <ryantf420@gmail.com>
2024-08-24 19:07:29 -0400

    Increase quickswitch prop priority Signed-off-by: minaripenguin <minaripenguin@users.noreply.github.com>

--- product.mk
+++ product.mk
@@ -6,19 +6,17 @@ PRODUCT_PACKAGES += \
     Lawnicons
 
 # Lawnchair Launcher
-PRODUCT_SYSTEM_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.quickswitch_lawnchair_shipped=1
-else 
+else
 # Lawnchair Launcher
-PRODUCT_SYSTEM_PROPERTIES += \
+PRODUCT_PRODUCT_PROPERTIES += \
     persist.sys.quickswitch_lawnchair_shipped=0
 endif
 
 # Basic call recorder
-ifeq ($(strip $(TARGET_PREBUILT_BCR)),true)
 PRODUCT_PACKAGES += \
     Bcr
-endif
 
 # Themed Icons
 $(call inherit-product, vendor/google/overlays/ThemeIcons/config.mk)
